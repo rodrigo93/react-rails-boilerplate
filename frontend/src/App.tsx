6384c2b514edb19e14f9e00d06ec45cf5260b417
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-08-25 13:51:04 -0300

    Changed React App to TypeScript with Jest

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 // TODO: Replace this with the correct URL for your API
 const API_URL = "http://localhost:3000/api/v1";
 
 // TODO: Update this function to call your API
 function getApiData() {
-    return axios.get(API_URL).then(response => response.data);
+  return axios.get(`${API_URL}`).then((response: { data: JSON; }) => response.data);
 }
 
 function App() {
@@ -15,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
